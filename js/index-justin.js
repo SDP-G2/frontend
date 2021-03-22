d08b4ec08c8597c37e115ddf3cb1a85b0781d36f
luchiee <luciagv-1999@hotmail.com>
2021-03-22 10:54:43 +0000

    API endpoints plus local storage Updating the registration user creation, as well as linking dashboard to the user and setting commands

--- js/index-justin.js
+++ js/index-justin.js
@@ -13,3 +13,87 @@ nav.onclick = function() {
         nav.style.left = '6.5rem';
     }
 }
+
+function create_robot(robot_name) {
+    const USER_ENDPOINT = "http://localhost:8000/admin/robot";
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "robot_serial_number": robot_name,
+            "assigned" : false
+        })
+    };
+
+    fetch(USER_ENDPOINT, params)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function create_user(username, password, serialnumber) {
+    const USER_ENDPOINT = "http://localhost:8000/user";
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "user_name" : username,
+            "password" : password,
+            "robot_serial_number": serialnumber
+        })
+    };
+
+    fetch(USER_ENDPOINT, params)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function get_authorisation() {
+    const USER_ENDPOINT = "http://localhost:8000/user";
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "user_name" : username,
+            "password" : password
+        })
+    };
+
+    fetch(USER_ENDPOINT, params)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+function start_now_testing(task_spec, auth){
+    var unix_time = Date.now();
+    if (task_spec == 1) {
+        task_string = "Circular";
+    }
+
+    const USER_ENDPOINT = "http://localhost:8000/command";
+    const params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : auth
+        },
+        body: JSON.stringify({
+            "robot_serial_number" : "serial2",
+            "time_issued": unix_time,
+            "time_instruction": [unix_time],
+            "instruction" : {
+                "Task": task_string
+            }
+        })
+    };
+
+    fetch(USER_ENDPOINT, params)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+}
